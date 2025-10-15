039e3d4b25fc80b8e047458d9a09339d4924deb5
amrrezzatt <amrezzat.acc2@gmail.com>
2025-10-15 12:16:37 +0300

    adding verify jwt file

---
+++ backend/middleware/verifyJWT.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const verifyJWT = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Access denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+
+    if (!user || user.activeToken !== token)
+      return res.status(403).json({ message: "Invalid or expired session" });
+
+    req.user = decoded; 
+    next();
+  } catch (err) {
+    res.status(403).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = verifyJWT;
