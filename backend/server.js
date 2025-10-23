cebb0d718194708de4e34b9750b46770316a3135
AliHRadwan <alihradwan@outlook.com>
2025-10-24 02:57:35 +0300

    creating the centralized error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,14 @@ import cartRoutes from "./routes/cartRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import authorRoutes from "./routes/authorRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-import reviewRoutes from "./routes/reviewRoutes.js"
+import reviewRoutes from "./routes/reviewRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -36,6 +37,9 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Node Project API" });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on: http://localhost:${port}`);
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on: http://localhost:${PORT}`);
 });
