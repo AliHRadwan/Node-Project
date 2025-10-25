54c2ce0f1bc4b3a9e7e6c6da9977cd6e0119aa51
aboelkhair <mo025731@gmail.com>
2025-10-25 23:43:04 +0300

    Final_Update_Cart_System

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,12 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 dotenv.config();
 connectDB();
 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(sessionMiddleware);
