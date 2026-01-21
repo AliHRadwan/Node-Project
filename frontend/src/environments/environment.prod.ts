6937e48a34642a5fe412ba35f4fa14d4611adbb6
AliHRadwan <alihradwan@outlook.com>
2026-01-22 00:41:41 +0200

    fixing the api base url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://18.184.165.152/api', 
-  siteName: 'Book Store'
+  apiUrl: 'https://bookly.duckdns.org/api', 
+  siteName: 'Bookly Bookstore'
 };
